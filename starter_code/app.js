ecc862c5e92a6afd9cb8fb8aa21a7349c88db446
Daniela Dionisio <dannyy.dionisio@gmail.com>
2019-11-24 22:11:02 +0000

    All done, just need style in tracks

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,6 +11,7 @@ app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
 
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
